e4f111b45c16127823e72ea7a72a2e74707c94af
luisalthoff <85774461+luisalthoff@users.noreply.github.com>
2021-06-25 02:14:46 -0300

    hh

--- 5.js
+++ 5.js
@@ -1,19 +1,23 @@
 
-var sqlite = require('sqlite3').verbose();
-var db = new sqlite.Database("./luis.db");
+function initDB()
+{
+    alert('ok');
+    var sqlite = require('sqlite3').verbose();
+    var db = new sqlite.Database("./luis.db");
 
-var matrixSignals = new Array();
-var i;
+    var matrixSignals = new Array();
+    var i;
 
-i = 0;
-db.all('SELECT * FROM alunos', function(err,rows){
-    rows.forEach(function(row) {
-        matrixSignals[i] = new Object();
-        matrixSignals[i].signalID = row.signalID;
-        matrixSignals[i].connID = row.connID;
-        i++;
+    i = 0;
+    db.all('SELECT * FROM alunos', function(err,rows){
+        rows.forEach(function(row) {
+            matrixSignals[i] = new Object();
+            matrixSignals[i].signalID = row.signalID;
+            matrixSignals[i].connID = row.connID;
+            i++;
+        });
+        db.close();
+        alert('1:' + matrixSignals.length);
     });
-    db.close();
-    alert('1:' + matrixSignals.length);
-});
+}
 //console.log('2:' + matrixSignals.length);
